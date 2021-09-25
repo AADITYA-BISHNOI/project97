e241a3eec7152fb796e5884d19a9eaf612484230
AADITYA-BISHNOI <73780850+AADITYA-BISHNOI@users.noreply.github.com>
2021-09-25 20:29:12 +0530

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,14 @@
+number = 1
+guess = 9
+print("Guess a number between 1 to 9: ")
+
+form = int(input("You have only one chance enter your number:- "))
+
+if (number==guess):
+    print("You win !!!!!!")
+if (number<guess):
+    print("you loose!!! the number was 9")
+else:
+    print("wrong input")
+
+  
